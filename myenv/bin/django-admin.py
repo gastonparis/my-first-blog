2803b9fa5e0b847ca6e22de7669ec7eb5dcbaec3
Gaston Paris <gastonparis@gmail.com>
2019-07-10 16:11:46 -0300

    Siguen los cambios por el stash

--- myenv/bin/django-admin.py
+++ myenv/bin/django-admin.py
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-#!/home/gaston/desarrollo/djangogirls/myenv/bin/python3.6
-=======
 #!/home/gaston/Documentos/djangogirls/myenv/bin/python
->>>>>>> Cambie el HTML para la página
+
 from django.core import management
 
 if __name__ == "__main__":
